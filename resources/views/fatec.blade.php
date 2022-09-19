f37f9d69c2d3548354127c7cfe2944e5c707dc4c
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2022-09-19 20:29:02 -0300

    First Commit!

---
+++ resources/views/fatec.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fatec Jales</title>
+</head>
+<body>
+    <div>
+        <h1>FATEC Jales</h1>
+        <hr>
+        <h2>Tecnologia em Sistemas para Internet</h2>
+    </div>
+</body>
+</html>
